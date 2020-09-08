9a334bd01db87d70b88310481faf86f8cf736030
Rohan Mehra <54217352+RohanMehraa@users.noreply.github.com>
2020-09-09 04:10:50 +0530

    Add initial files for node.js

---
+++ intro-to-node/index.js
@@ -0,0 +1,8 @@
+const sh = require("superheroes");
+const sv = require("supervillains");
+
+var mySuperHero = sh.random();
+var mySuperVillain = sv.random();
+
+console.log(mySuperHero);
+console.log(mySuperVillain);
